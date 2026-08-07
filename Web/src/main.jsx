a682572e93c0b0967e55f80dc255e39eeeaea902
Umer-Saleh <121632337+Umer-Saleh@users.noreply.github.com>
2026-08-07 17:29:03 +0300

    Add ambient backdrop: pixel grid, CRT scanlines, vignette
    Three fixed pointer-events:none layers behind the whole app, giving
    every screen the arcade-cabinet texture from the design in one place.

--- Web/src/main.jsx
+++ Web/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { VaultProvider } from './context/VaultContext';
 import { ThemeProvider } from './context/ThemeContext';
+import { Backdrop } from './components/Backdrop';
 import App from './App.jsx'
 import './index.css'
 import './theme.css';
@@ -9,6 +10,7 @@ import './theme.css';
 createRoot(document.getElementById('root')).render(
   <ThemeProvider>
     <VaultProvider>
+      <Backdrop />
       <App />
     </VaultProvider>
   </ThemeProvider>,
